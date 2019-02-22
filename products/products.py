24e60182d0b2c386924d73c04f6bd511d5db903d
Mask Wen <mask60113@gmail.com>
2019-02-22 15:51:22 +0800

    add csv encode utf-8

--- products/products.py
+++ products/products.py
@@ -10,6 +10,10 @@ print(products)
 for product in products:
 	print(product[0], '的價格為 ', product[1])
 
-with open('products.txt','w') as f:
+with open('products.csv','w',encoding='utf-8') as f:
+	f.write('商品名稱,價錢\n')
 	for p in products:
 		f.write(p[0] + ',' + p[1] + '\n')
+
+# Excel讀取標頭會亂碼，必須使用utf-8寫入
+# 但讀取時也要用utf-8，excel 要從Data 匯入，選擇utf-8並使用逗號做分割
